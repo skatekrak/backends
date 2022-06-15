1c9c2823e6114c8ab2de699db886b155359aa303
gfanton <8671905+gfanton@users.noreply.github.com>
2022-06-15 10:45:17 +0200

    correctly handle database error
    Signed-off-by: gfanton <8671905+gfanton@users.noreply.github.com>

--- database/database.go
+++ database/database.go
@@ -1,17 +1,10 @@
 package database
 
 import (
-	"log"
-
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-func Database() (*gorm.DB, error) {
-	db, err := gorm.Open(sqlite.Open("./local.db"), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	return db, err
+func Open(path string) (*gorm.DB, error) {
+	return gorm.Open(sqlite.Open(path), &gorm.Config{})
 }
