1be5c89057233802e1bcdc1388dda0ef95aabdf1
Maxime Cattet <maximecattet@gmx.com>
2022-07-22 16:02:16 +0200

    feat: Init new Carrelage API

---
+++ carrelage/go.mod
@@ -0,0 +1,16 @@
+module github.com/skatekrak/carrelage
+
+go 1.18
+
+replace github.com/skatekrak/utils v0.0.0 => ../utils
+
+require github.com/gofiber/fiber/v2 v2.35.0
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.38.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220721230656-c6bc011c0c49 // indirect
+)
---
+++ carrelage/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+)
+
+func main() {
+	app := fiber.New()
+
+	// Setup middlewares
+	app.Use(logger.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: os.Getenv("CORS_ORIGINS"),
+	}))
+	app.Use(compress.New())
+	app.Use(recover.New())
+
+	// Start server
+	if err := app.Listen(fmt.Sprintf(":%s", os.Getenv("PORT"))); err != nil {
+		log.Fatalln("Error listening")
+		log.Fatalln(err)
+	}
+}
